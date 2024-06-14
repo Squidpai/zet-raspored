074bab875ca83bc53b67890f6773e1fd3d4d33e8
Squidpai <leonardzadro@gmail.com>
2024-06-14 17:44:34 +0200

    Updated libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,12 +49,12 @@ android {
 dependencies {
 
   implementation("androidx.core:core-ktx:1.13.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
   implementation("androidx.activity:activity-compose:1.9.0")
   implementation(platform("androidx.compose:compose-bom:2024.02.01"))
-  implementation("androidx.compose.ui:ui:1.6.7")
-  implementation("androidx.compose.ui:ui-graphics:1.6.7")
-  implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
+  implementation("androidx.compose.ui:ui:1.6.8")
+  implementation("androidx.compose.ui:ui-graphics:1.6.8")
+  implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
   implementation("androidx.compose.material3:material3:1.2.1")
   implementation("com.opencsv:opencsv:5.9")
   implementation("org.mobilitydata:gtfs-realtime-bindings:0.0.8")
@@ -62,7 +62,7 @@ dependencies {
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
   androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
-  androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
-  debugImplementation("androidx.compose.ui:ui-tooling:1.6.7")
-  debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
+  androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.8")
+  debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
+  debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.8")
 }
