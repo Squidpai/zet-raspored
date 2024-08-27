a1224278a40925b88745fb6a2abc899bbb3d03b6
Squidpai <120787088+Squidpai@users.noreply.github.com>
2024-08-27 14:10:21 +0200

    Updated for version 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
       applicationId = "hr.squidpai.zetlive"
       minSdk = 26
       targetSdk = 35
-      versionCode = 6
-      versionName = "0.5"
+      versionCode = 7
+      versionName = "0.5.1"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
       vectorDrawables {
