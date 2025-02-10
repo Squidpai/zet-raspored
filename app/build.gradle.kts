90a7082fc7c81f0fe5992cd094f4c986e289b7b4
Squidpai <120787088+Squidpai@users.noreply.github.com>
2025-02-10 11:08:48 +0100

    Updated version to 0.8.3, dependencies, compile SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
    namespace = "hr.squidpai.zetlive"
-   compileSdk = 34
+   compileSdk = 35
 
    defaultConfig {
       applicationId = "hr.squidpai.zetlive"
       minSdk = 26
       targetSdk = 35
-      versionCode = 16
-      versionName = "0.8.2"
+      versionCode = 17
+      versionName = "0.8.3"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
       vectorDrawables {
@@ -49,22 +49,22 @@ android {
 
 dependencies {
    implementation(project(":zetapi"))
-   implementation("androidx.core:core-ktx:1.13.1")
-   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-   implementation("androidx.activity:activity-compose:1.9.2")
-   implementation(platform("androidx.compose:compose-bom:2024.09.02"))
-   implementation("androidx.compose.ui:ui:1.7.2")
-   implementation("androidx.compose.ui:ui-graphics:1.7.2")
-   implementation("androidx.compose.ui:ui-tooling-preview:1.7.2")
-   implementation("androidx.compose.material3:material3:1.3.0")
+   implementation("androidx.core:core-ktx:1.15.0")
+   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+   implementation("androidx.activity:activity-compose:1.10.0")
+   implementation(platform("androidx.compose:compose-bom:2025.01.01"))
+   implementation("androidx.compose.ui:ui:1.7.7")
+   implementation("androidx.compose.ui:ui-graphics:1.7.7")
+   implementation("androidx.compose.ui:ui-tooling-preview:1.7.7")
+   implementation("androidx.compose.material3:material3:1.3.1")
    implementation("com.google.android.play:app-update:2.1.0")
    implementation("com.google.android.play:app-update-ktx:2.1.0")
    implementation("androidx.lifecycle:lifecycle-process:2.8.7")
    testImplementation("junit:junit:4.13.2")
    androidTestImplementation("androidx.test.ext:junit:1.2.1")
    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-   androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.02"))
-   androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.2")
-   debugImplementation("androidx.compose.ui:ui-tooling:1.7.2")
-   debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.2")
+   androidTestImplementation(platform("androidx.compose:compose-bom:2025.01.01"))
+   androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.7")
+   debugImplementation("androidx.compose.ui:ui-tooling:1.7.7")
+   debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.7")
 }
