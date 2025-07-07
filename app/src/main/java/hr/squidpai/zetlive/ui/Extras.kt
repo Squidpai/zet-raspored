ad5ac3387a295cca2a941af04734e347357b9d6a
Squidpai <120787088+Squidpai@users.noreply.github.com>
2025-07-07 13:48:13 +0200

    Added EXTRA_STOP

--- app/src/main/java/hr/squidpai/zetlive/ui/Extras.kt
+++ app/src/main/java/hr/squidpai/zetlive/ui/Extras.kt
@@ -2,6 +2,7 @@ package hr.squidpai.zetlive.ui
 
 private const val ROOT = "hr.squidpai.zetlive.extra"
 const val EXTRA_ROUTE_ID = "$ROOT.ROUTE_ID"
+const val EXTRA_STOP = "hr.squidpai.zetlive.extra.STOP"
 const val EXTRA_DIRECTION = "$ROOT.DIRECTION"
 const val EXTRA_TRIP_ID = "$ROOT.TRIP_ID"
 const val EXTRA_SELECTED_DATE = "$ROOT.SELECTED_DATE"
