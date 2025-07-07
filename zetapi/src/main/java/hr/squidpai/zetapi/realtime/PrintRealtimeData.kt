775b07d032a4678ca44849f752e05ac21d8d37cd
Squidpai <120787088+Squidpai@users.noreply.github.com>
2025-07-07 14:04:52 +0200

    Added additional things to print for debugging purposes.

--- zetapi/src/main/java/hr/squidpai/zetapi/realtime/PrintRealtimeData.kt
+++ zetapi/src/main/java/hr/squidpai/zetapi/realtime/PrintRealtimeData.kt
@@ -1,5 +1,30 @@
 package hr.squidpai.zetapi.realtime
 
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 private fun main() {
-   println(RealtimeDispatcher.download())
+   val data = RealtimeDispatcher.download()
+   val timestamp = ZonedDateTime.ofInstant(
+      Instant.ofEpochSecond(data.header.timestamp),
+      ZoneId.of("Europe/Zagreb"),
+   ).format(
+      DateTimeFormatter.ofPattern("dd.MM.yyyy. HH:mm:ss")
+   )
+   println("Feed timestamp: $timestamp")
+   println(data)
+
+   val tripsWithTripUpdate = data.entityList.filter { it.hasTripUpdate() }
+      .map { it.tripUpdate.trip.tripId }
+      .toSet()
+
+   val tripsWithVehicleUpdates = data.entityList.filter { it.hasVehicle() }
+      .map { it.vehicle.trip.tripId }
+      .toSet()
+
+   val tripsWithVehicleUpdatesWithoutTripUpdates = tripsWithVehicleUpdates - tripsWithTripUpdate
+
+   println("Trips with trip updates without vehicle updates: $tripsWithVehicleUpdatesWithoutTripUpdates")
 }
