7c58dda1f889f969895cb59a71f94b4cc76f0818
Squidpai <leonardzadro@gmail.com>
2025-07-28 20:07:15 +0200

    Updated source to Java 21, updated some libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,11 @@ android {
       }
    }
    compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_1_8
-      targetCompatibility = JavaVersion.VERSION_1_8
+      sourceCompatibility = JavaVersion.VERSION_21
+      targetCompatibility = JavaVersion.VERSION_21
    }
    kotlinOptions {
-      jvmTarget = "1.8"
+      jvmTarget = "21"
    }
    buildFeatures {
       compose = true
@@ -50,20 +50,20 @@ android {
 dependencies {
    implementation(project(":zetapi"))
    implementation("androidx.core:core-ktx:1.16.0")
-   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
    implementation("androidx.activity:activity-compose:1.10.1")
-   implementation(platform("androidx.compose:compose-bom:2025.06.01"))
+   implementation(platform("androidx.compose:compose-bom:2025.07.00"))
    implementation("androidx.compose.ui:ui:1.8.3")
    implementation("androidx.compose.ui:ui-graphics:1.8.3")
    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
    implementation("androidx.compose.material3:material3:1.3.2")
    //implementation("com.google.android.play:app-update:2.1.0")
    //implementation("com.google.android.play:app-update-ktx:2.1.0")
-   implementation("androidx.lifecycle:lifecycle-process:2.9.1")
+   implementation("androidx.lifecycle:lifecycle-process:2.9.2")
    testImplementation("junit:junit:4.13.2")
    androidTestImplementation("androidx.test.ext:junit:1.2.1")
    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-   androidTestImplementation(platform("androidx.compose:compose-bom:2025.06.01"))
+   androidTestImplementation(platform("androidx.compose:compose-bom:2025.07.00"))
    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.3")
    debugImplementation("androidx.compose.ui:ui-tooling:1.8.3")
    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.3")
