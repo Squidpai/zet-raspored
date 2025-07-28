74154d9f097458d319b1c3d11ad6030cd316c373
Squidpai <leonardzadro@gmail.com>
2025-07-28 20:07:31 +0200

    Updated source to Java 21, updated some libraries, added YAML library

--- zetapi/build.gradle.kts
+++ zetapi/build.gradle.kts
@@ -5,18 +5,18 @@ plugins {
 }
 
 java {
-   sourceCompatibility = JavaVersion.VERSION_1_8
-   targetCompatibility = JavaVersion.VERSION_1_8
+   sourceCompatibility = JavaVersion.VERSION_21
+   targetCompatibility = JavaVersion.VERSION_21
 }
 
 kotlin {
-   jvmToolchain(8)
    explicitApi()
 }
 
 dependencies {
-   implementation("com.opencsv:opencsv:5.9")
+   implementation("com.opencsv:opencsv:5.11.2")
    implementation("org.mobilitydata:gtfs-realtime-bindings:0.0.8")
-   implementation("androidx.collection:collection:1.4.5")
-   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+   implementation("androidx.collection:collection:1.5.0")
+   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+   implementation("com.charleskorn.kaml:kaml:0.85.0")
 }
