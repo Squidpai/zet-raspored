7a627dbe3ce199f8cf181d7fba908d9372bd0139
Squidpai <leonardzadro@gmail.com>
2025-07-29 17:00:57 +0200

    Made longName automatically have en-dashes added to it.

--- zetapi/src/main/java/hr/squidpai/zetapi/gtfs/GtfsRoute.kt
+++ zetapi/src/main/java/hr/squidpai/zetapi/gtfs/GtfsRoute.kt
@@ -9,45 +9,48 @@ import hr.squidpai.zetapi.TripId
 import hr.squidpai.zetapi.csvToListFromEntry
 import java.util.zip.ZipFile
 
+private val spaceHyphenSpaceRegex = Regex(" *- *")
+
 internal class GtfsRoute(
-   headerMap: IntArray,
-   data: Array<out String>,
+    headerMap: IntArray,
+    data: Array<out String>,
 ) : Route(
-   id = data[headerMap[0]],
-   shortName = data.getOrNull(headerMap[1]).orEmpty(),
-   longName = data.getOrNull(headerMap[2]).orEmpty(),
-   type = Type(data.getOrNull(headerMap[3])),
-   sortOrder = data.getOrNull(headerMap[4])?.toIntOrNull()
-      ?: data[headerMap[0]].let { id ->
-         id.filter { it.isDigit() }.toIntOrNull() ?: id.hashCode()
-      },
-   commonHeadsigns = GtfsRouteCommonHeadsigns(),
+    id = data[headerMap[0]],
+    shortName = data.getOrNull(headerMap[1]).orEmpty(),
+    longName = data.getOrNull(headerMap[2])
+        // TODO i dont really like this, may remove it or move it to CachedRoute or smth
+        ?.replace(spaceHyphenSpaceRegex, " ${Typography.ndash} ").orEmpty(),
+    type = Type(data.getOrNull(headerMap[3])),
+    sortOrder = data.getOrNull(headerMap[4])?.toIntOrNull()
+        ?: data[headerMap[0]].let { id ->
+            id.filter { it.isDigit() }.toIntOrNull() ?: id.hashCode()
+        },
+    commonHeadsigns = GtfsRouteCommonHeadsigns(),
 ) {
-   override var trips = HashMap<TripId, Trip>()
-      internal set
+    override var trips = HashMap<TripId, Trip>()
+        internal set
 
-   override val stopSequences =
-      mutableListOf<List<Stop>>() to mutableListOf<List<Stop>>()
+    override val stopSequences =
+        mutableListOf<List<Stop>>() to mutableListOf<List<Stop>>()
 
-   companion object {
-      fun loadRoutes(zip: ZipFile) =
-         zip.csvToListFromEntry("routes.txt", mapping, ::GtfsRoute)
+    companion object {
+        fun loadRoutes(zip: ZipFile) =
+            zip.csvToListFromEntry("routes.txt", mapping, ::GtfsRoute)
 
-      private val mapping: CsvHeaderMapping = { header ->
-         val headerMap = IntArray(5) { -1 }
-         header.forEachIndexed { i, h ->
-            when (h) {
-               "route_id" -> headerMap[0] = i
-               "route_short_name" -> headerMap[1] = i
-               "route_long_name" -> headerMap[2] = i
-               "route_type" -> headerMap[3] = i
-               "route_sort_order" -> headerMap[4] = i
+        private val mapping: CsvHeaderMapping = { header ->
+            val headerMap = IntArray(5) { -1 }
+            header.forEachIndexed { i, h ->
+                when (h) {
+                    "route_id" -> headerMap[0] = i
+                    "route_short_name" -> headerMap[1] = i
+                    "route_long_name" -> headerMap[2] = i
+                    "route_type" -> headerMap[3] = i
+                    "route_sort_order" -> headerMap[4] = i
+                }
             }
-         }
-         headerMap
-      }
-   }
+            headerMap
+        }
+    }
 }
 
-internal typealias GtfsRouteCommonHeadsigns =
-      HashMap<ServiceId, Pair<String, String>>
+internal typealias GtfsRouteCommonHeadsigns = HashMap<ServiceId, Pair<String, String>>
