cfc83e24d337b8650467d437cb2884b8bcf90109
Squidpai <leonardzadro@gmail.com>
2025-07-29 17:34:07 +0200

    Downloading the schedule now sends a HEAD request first, saving bandwidth, also stops are now loaded with the new stop mapper.

--- zetapi/src/main/java/hr/squidpai/zetapi/gtfs/GtfsScheduleLoader.kt
+++ zetapi/src/main/java/hr/squidpai/zetapi/gtfs/GtfsScheduleLoader.kt
@@ -9,6 +9,7 @@ import hr.squidpai.zetapi.realtime.RealtimeDispatcher
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URI
 import java.net.URISyntaxException
@@ -16,143 +17,182 @@ import java.util.zip.ZipFile
 
 public object GtfsScheduleLoader {
 
-   public const val LINK: String = "https://www.zet.hr/gtfs-scheduled/latest"
+    public const val LINK: String = "https://www.zet.hr/gtfs-scheduled/latest"
 
-   public fun versionLink(version: String): String =
-      "https://www.zet.hr/gtfs-scheduled/scheduled-000-$version.zip"
+    public fun versionLink(version: String): String =
+        "https://www.zet.hr/gtfs-scheduled/scheduled-000-$version.zip"
 
-   public data class DownloadResult(
-      val errorType: ErrorType? = null,
-      val exception: Exception? = null,
-      val version: String? = null,
-   ) {
-      public companion object {
-         public val SUCCESS: DownloadResult = DownloadResult()
-      }
-   }
+    public data class DownloadResult(
+        val errorType: ErrorType? = null,
+        val exception: Exception? = null,
+        val version: String? = null,
+    ) {
+        public companion object {
+            public val SUCCESS: DownloadResult = DownloadResult()
+        }
+    }
 
-   /**
-    * Downloads the GTFS schedule from the specified [link] to [file].
-    *
-    * The schedule won't be downloaded if its version matches [cachedVersion].
-    */
-   public fun download(
-      file: File,
-      vararg cachedVersions: String?,
-      link: String = LINK,
-      onDownloadProgress: (Float) -> Unit = { },
-   ): DownloadResult {
-      val url = try {
-         URI(link).toURL()
-      } catch (e: MalformedURLException) {
-         return DownloadResult(ErrorType.MALFORMED_URL, e)
-      } catch (e: URISyntaxException) {
-         return DownloadResult(ErrorType.MALFORMED_URL, e)
-      }
+    /**
+     * Downloads the GTFS schedule from the specified [link] to [file].
+     *
+     * The schedule won't be downloaded if its version matches [cachedVersion].
+     */
+    private fun download0(
+        useHeadMethod: Boolean,
+        file: File,
+        vararg cachedVersions: String?,
+        link: String = LINK,
+        onDownloadProgress: (Float) -> Unit = { },
+    ): DownloadResult {
+        val url = try {
+            URI(link).toURL()
+        } catch (e: MalformedURLException) {
+            return DownloadResult(ErrorType.MALFORMED_URL, e)
+        } catch (e: URISyntaxException) {
+            return DownloadResult(ErrorType.MALFORMED_URL, e)
+        }
 
-      val connection = try {
-         url.openConnection().also { it.connect() }
-      } catch (e: IOException) {
-         return DownloadResult(ErrorType.OPENING_CONNECTION, e)
-      }
+        val connection = try {
+            url.openConnection()
+        } catch (e: IOException) {
+            return DownloadResult(ErrorType.OPENING_CONNECTION, e)
+        }
 
-      val fieldValue = connection.getHeaderField("Content-Disposition")
-      // no content, do not download anything
-         ?: return DownloadResult(ErrorType.NO_CONTENT)
+        if (useHeadMethod)
+            (connection as HttpURLConnection).requestMethod = "HEAD"
 
-      val index = fieldValue.indexOf("filename=")
-      if (index == -1)
-      // no file name, do not download
-         return DownloadResult(ErrorType.NO_FILENAME)
+        try {
+            connection.connect()
+        } catch (e: IOException) {
+            return DownloadResult(ErrorType.OPENING_CONNECTION, e)
+        }
 
-      val newVersion = FeedInfo.versionFromFileName(
-         fieldValue
-            .substring(index + 9)
-            .replace("\"", "")
-      )
+        val fieldValue = connection.getHeaderField("Content-Disposition")
+        // no content, do not download anything
+            ?: return DownloadResult(ErrorType.NO_CONTENT)
 
-      val contentLength = connection.contentLength.toFloat()
+        val index = fieldValue.indexOf("filename=")
+        if (index == -1)
+        // no file name, do not download
+            return DownloadResult(ErrorType.NO_FILENAME)
 
-      val latestVersion = maxOf(*cachedVersions)
+        val newVersion = FeedInfo.versionFromFileName(
+            fieldValue
+                .substring(index + 9)
+                .replace("\"", "")
+        )
 
-      if (latestVersion != null && latestVersion >= newVersion)
-         return DownloadResult(ErrorType.UP_TO_DATE)
-      else try {
-         val buffer = ByteArray(4096)
-         connection.inputStream.use { input ->
-            FileOutputStream(file).use { output ->
-               var count: Int
-               while (input.read(buffer).also { count = it } != -1) {
-                  output.write(buffer, 0, count)
-                  onDownloadProgress(count / contentLength)
-               }
+        val contentLength = connection.contentLength.toFloat()
+
+        val latestVersion = maxOf(*cachedVersions)
+
+        if (latestVersion != null && latestVersion >= newVersion)
+            return DownloadResult(ErrorType.UP_TO_DATE)
+        else if (useHeadMethod)
+            // there is no body, simply send a successful response
+            return DownloadResult(version = latestVersion)
+        else try {
+            val buffer = ByteArray(4096)
+            connection.inputStream.use { input ->
+                FileOutputStream(file).use { output ->
+                    var count: Int
+                    while (input.read(buffer).also { count = it } != -1) {
+                        output.write(buffer, 0, count)
+                        onDownloadProgress(count / contentLength)
+                    }
+                }
             }
-         }
-      } catch (e: IOException) {
-         // failed to download the new version,
-         // delete the incomplete schedule.
-         file.delete()
-         return DownloadResult(ErrorType.DOWNLOAD_ERROR, e, newVersion)
-      }
+        } catch (e: IOException) {
+            // failed to download the new version,
+            // delete the incomplete schedule.
+            file.delete()
+            return DownloadResult(ErrorType.DOWNLOAD_ERROR, e, newVersion)
+        }
 
-      return DownloadResult(version = newVersion)
-   }
+        return DownloadResult(version = newVersion)
+    }
 
-   /** Loads the schedule in [gtfsScheduleZip]. */
-   public fun load(
-      gtfsScheduleZip: File,
-      realtimeDispatcher: RealtimeDispatcher,
-      onLoadProgress: (Float) -> Unit,
-   ): Schedule {
-      ZipFile(gtfsScheduleZip).use { zip ->
-         val feedInfo = FeedInfo.fromZip(zip)
-         val routes = GtfsRoute.loadRoutes(zip)
-            .apply { sortBy { it.sortOrder } }
-            .associateBy { it.id }
-         onLoadProgress(.01f)
+    /**
+     * Downloads the GTFS schedule from the specified [link] to [file].
+     *
+     * The schedule won't be downloaded if its version matches [cachedVersion].
+     */
+    public fun download(
+        file: File,
+        vararg cachedVersions: String?,
+        link: String = LINK,
+        onDownloadProgress: (Float) -> Unit = { },
+    ): DownloadResult {
+        // first just use the head method to see if there is a new version
+        // doing a HEAD request saves around 700 kB of bandwidth if there's no version
+        val headResult = download0(
+            useHeadMethod = true, file, cachedVersions = cachedVersions, link, onDownloadProgress
+        )
+        // no new version, or error occurred: return head result
+        if (headResult.version == null)
+            return headResult
 
-         val stops = Stops(loadStops(zip))
-         onLoadProgress(.07f)
+        // new version found: download it (also checks headers again but whatever)
+        return download0(
+            useHeadMethod = false, file, cachedVersions = cachedVersions, link, onDownloadProgress
+        )
+    }
 
-         var shapes = loadShapes(zip)
-         onLoadProgress(.30f)
+    /** Loads the schedule in [gtfsScheduleZip]. */
+    public fun load(
+        gtfsScheduleZip: File,
+        realtimeDispatcher: RealtimeDispatcher,
+        onLoadProgress: (Float) -> Unit,
+    ): Schedule {
+        ZipFile(gtfsScheduleZip).use { zip ->
+            val feedInfo = FeedInfo.fromZip(zip)
+            val routes = GtfsRoute.loadRoutes(zip)
+                .apply { sortBy { it.sortOrder } }
+                .associateBy { it.id }
+            onLoadProgress(.01f)
 
-         val fauxShapes = GtfsTripLoader.loadTrips(
-            zip,
-            routes,
-            stops,
-            shapes,
-            realtimeDispatcher,
-            onLoadProgress,
-         )
-         if (shapes is MutableMap)
-            shapes.putAll(fauxShapes)
-         else
-            shapes = shapes + fauxShapes
+            val stops = Stops(Love.giveMeBetterStops(loadStops(zip)))
+            onLoadProgress(.07f)
 
-         val calendarDates = loadCalendarDates(zip)
-         val serviceIdTypes = Love.giveMeTheServiceIdTypes(routes, calendarDates)
+            var shapes = loadShapes(zip)
+            onLoadProgress(.30f)
 
-         onLoadProgress(Float.NaN)
+            val fauxShapes = GtfsTripLoader.loadTrips(
+                zip,
+                routes,
+                stops,
+                shapes,
+                realtimeDispatcher,
+                onLoadProgress,
+            )
+            if (shapes is MutableMap)
+                shapes.putAll(fauxShapes)
+            else
+                shapes = shapes + fauxShapes
 
-         return Schedule(
-            feedInfo,
-            routes,
-            stops,
-            shapes,
-            calendarDates,
-            serviceIdTypes,
-         )
-      }
-   }
+            val calendarDates = loadCalendarDates(zip)
+            val serviceIdTypes = Love.giveMeTheServiceIdTypes(routes, calendarDates)
 
-   public fun getFeedInfo(file: File): FeedInfo =
-      ZipFile(file).use { FeedInfo.fromZip(it) }
+            onLoadProgress(Float.NaN)
 
-   public fun getFeedInfoOrNull(file: File): FeedInfo? = try {
-   	getFeedInfo(file)
-   } catch (_: Exception) {
-      null
-   }
+            return Schedule(
+                feedInfo,
+                routes,
+                stops,
+                shapes,
+                calendarDates,
+                serviceIdTypes,
+            )
+        }
+    }
+
+    public fun getFeedInfo(file: File): FeedInfo =
+        ZipFile(file).use { FeedInfo.fromZip(it) }
+
+    public fun getFeedInfoOrNull(file: File): FeedInfo? = try {
+        getFeedInfo(file)
+    } catch (_: Exception) {
+        null
+    }
 
 }
