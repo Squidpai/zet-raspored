76afd66b61955358c534c2e2e84d5dab3c8d61fd
Squidpai <leonardzadro@gmail.com>
2025-07-29 17:35:47 +0200

    Added a note.

--- zetapi/src/main/java/hr/squidpai/zetapi/realtime/RealtimeData.kt
+++ zetapi/src/main/java/hr/squidpai/zetapi/realtime/RealtimeData.kt
@@ -17,115 +17,118 @@ import kotlin.time.Duration.Companion.seconds
 /** Object containing useful real time data for a trip. */
 public sealed interface RealtimeData {
 
-   /**
-    * This trip has a [TripUpdate]. Use the [get] operator to fetch
-    * the delay for a specific stop, or [applyDelays] to apply delays to
-    * [Trip.departures] using linear interpolation.
-    */
-   public class DelayByStop(public val tripUpdate: TripUpdate) : RealtimeData {
+    /**
+     * This trip has a [TripUpdate]. Use the [get] operator to fetch
+     * the delay for a specific stop, or [applyDelays] to apply delays to
+     * [Trip.departures] using linear interpolation.
+     */
+    public class DelayByStop(public val tripUpdate: TripUpdate) : RealtimeData {
 
-      private inline val StopTimeUpdate.stopIndex get() = stopSequence - 1
+        // NOTE: if at any point StopTimeUpdate.stopId should be read,
+        //       a new function in Love will need to be created to map to the better stop id.
 
-      public operator fun get(stopIndex: Int): Duration =
-         (tripUpdate.stopTimeUpdateList
-            .lastOrNull { it.stopIndex <= stopIndex }
-            ?: tripUpdate.stopTimeUpdateList.first()).departure.delay.seconds
+        private inline val StopTimeUpdate.stopIndex get() = stopSequence - 1
 
-      public fun applyDelays(departures: TimeOfDayList): TimeOfDayList {
-         if (tripUpdate.stopTimeUpdateList.isEmpty())
-            return departures
+        public operator fun get(stopIndex: Int): Duration =
+            (tripUpdate.stopTimeUpdateList
+                .lastOrNull { it.stopIndex <= stopIndex }
+                ?: tripUpdate.stopTimeUpdateList.first()).departure.delay.seconds
 
-         val realtimeDepartures = MutableIntList(departures.size)
+        public fun applyDelays(departures: TimeOfDayList): TimeOfDayList {
+            if (tripUpdate.stopTimeUpdateList.isEmpty())
+                return departures
 
-         val sortedUpdateList = tripUpdate.stopTimeUpdateList
-            .sortedBy { it.stopIndex }
+            val realtimeDepartures = MutableIntList(departures.size)
 
-         val iterator = sortedUpdateList.iterator()
-         var previous = iterator.next()
+            val sortedUpdateList = tripUpdate.stopTimeUpdateList
+                .sortedBy { it.stopIndex }
 
-         for (i in 0..previous.stopIndex)
-            realtimeDepartures += departures[i] + previous.departure.delay
+            val iterator = sortedUpdateList.iterator()
+            var previous = iterator.next()
 
-         while (iterator.hasNext()) {
-            val current = iterator.next()
-            val totalTime = (departures[current.stopIndex] -
-                  departures[previous.stopIndex]).toFloat()
-            var time = 0
-            for (i in previous.stopIndex + 1..current.stopIndex) {
-               time += departures[i] - departures[i - 1]
-               realtimeDepartures += departures[i] + lerp(
-                  previous.departure.delay,
-                  current.departure.delay,
-                  fraction = time / totalTime,
-               )
+            for (i in 0..previous.stopIndex)
+                realtimeDepartures += departures[i] + previous.departure.delay
+
+            while (iterator.hasNext()) {
+                val current = iterator.next()
+                val totalTime = (departures[current.stopIndex] -
+                        departures[previous.stopIndex]).toFloat()
+                var time = 0
+                for (i in previous.stopIndex + 1..current.stopIndex) {
+                    time += departures[i] - departures[i - 1]
+                    realtimeDepartures += departures[i] + lerp(
+                        previous.departure.delay,
+                        current.departure.delay,
+                        fraction = time / totalTime,
+                    )
+                }
+                previous = current
             }
-            previous = current
-         }
 
-         for (i in previous.stopIndex + 1..departures.lastIndex)
-            realtimeDepartures += departures[i] + previous.departure.delay
+            for (i in previous.stopIndex + 1..departures.lastIndex)
+                realtimeDepartures += departures[i] + previous.departure.delay
 
-         return realtimeDepartures
-      }
+            return realtimeDepartures
+        }
 
-      override fun toString(): String =
-         tripUpdate.stopTimeUpdateList
-            .joinToString(prefix = "DelayByStop{", postfix = "}") {
-               "${it.stopIndex}: ${it.departure.delay}"
+        override fun toString(): String =
+            tripUpdate.stopTimeUpdateList
+                .joinToString(prefix = "DelayByStop{", postfix = "}") {
+                    "${it.stopIndex}: ${it.departure.delay}"
+                }
+    }
+
+    /**
+     * This trip has a [VehiclePosition]. It contains the
+     * [latitude] and the [longitude] of the vehicle.
+     */
+    public class Position(
+        public val vehiclePosition: VehiclePosition
+    ) : RealtimeData {
+        public val latitude: Float get() = vehiclePosition.position.latitude
+
+        public val longitude: Float get() = vehiclePosition.position.longitude
+
+        override fun toString(): String =
+            "VehiclePosition(latitude=$latitude, longitude=$longitude)"
+    }
+
+    /** This trip is cancelled. */
+    public data object Cancelled : RealtimeData
+
+    /** This trip contains some other [Alert] which we don't recognise. */
+    public class OtherAlert(public val alert: Alert) : RealtimeData
+
+    /** This trip contains no realtime data associated with it. */
+    public data object None : RealtimeData
+
+    public companion object {
+        public fun fromEntity(entity: FeedEntity, tripId: TripId): RealtimeData {
+            if (entity.hasTripUpdate() && entity.tripUpdate.trip.tripId == tripId)
+                return DelayByStop(entity.tripUpdate)
+            if (entity.hasVehicle() && entity.vehicle.trip.tripId == tripId)
+                return Position(entity.vehicle)
+            if (entity.hasAlert() && entity.alert.informedEntityList
+                    .any { it.trip.tripId == tripId }
+            ) {
+                if (entity.alert.effect == Alert.Effect.NO_SERVICE)
+                    return Cancelled
+                return OtherAlert(entity.alert)
             }
-   }
+            return None
+        }
 
-   /**
-    * This trip has a [VehiclePosition]. It contains the
-    * [latitude] and the [longitude] of the vehicle.
-    */
-   public class Position(
-      public val vehiclePosition: VehiclePosition
-   ) : RealtimeData {
-      public val latitude: Float get() = vehiclePosition.position.latitude
+        public fun searchForLinearly(
+            feedMessage: FeedMessage,
+            tripId: TripId,
+        ): RealtimeData {
+            for (entity in feedMessage.entityList) {
+                val data = fromEntity(entity, tripId)
+                if (data != None)
+                    return data
+            }
 
-      public val longitude: Float get() = vehiclePosition.position.longitude
-
-      override fun toString(): String =
-         "VehiclePosition(latitude=$latitude, longitude=$longitude)"
-   }
-
-   /** This trip is cancelled. */
-   public data object Cancelled : RealtimeData
-
-   /** This trip contains some other [Alert] which we don't recognise. */
-   public class OtherAlert(public val alert: Alert) : RealtimeData
-
-   /** This trip contains no realtime data associated with it. */
-   public data object None : RealtimeData
-
-   public companion object {
-      public fun fromEntity(entity: FeedEntity, tripId: TripId): RealtimeData {
-         if (entity.hasTripUpdate() && entity.tripUpdate.trip.tripId == tripId)
-            return DelayByStop(entity.tripUpdate)
-         if (entity.hasVehicle() && entity.vehicle.trip.tripId == tripId)
-            return Position(entity.vehicle)
-         if (entity.hasAlert() && entity.alert.informedEntityList
-               .any { it.trip.tripId == tripId }
-         ) {
-            if (entity.alert.effect == Alert.Effect.NO_SERVICE)
-               return Cancelled
-            return OtherAlert(entity.alert)
-         }
-         return None
-      }
-
-      public fun searchForLinearly(
-         feedMessage: FeedMessage,
-         tripId: TripId,
-      ): RealtimeData {
-         for (entity in feedMessage.entityList) {
-            val data = fromEntity(entity, tripId)
-            if (data != None)
-               return data
-         }
-
-         return None
-      }
-   }
+            return None
+        }
+    }
 }
