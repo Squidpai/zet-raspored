b96b5a84a5424f4efec0031d232b17b3189a07da
Squidpai <leonardzadro@gmail.com>
2025-07-29 17:38:06 +0200

    Added new constructor.

--- zetapi/src/main/java/hr/squidpai/zetapi/RouteAtStop.kt
+++ zetapi/src/main/java/hr/squidpai/zetapi/RouteAtStop.kt
@@ -3,19 +3,31 @@ package hr.squidpai.zetapi
 /** Class containing information about a route that passes through a stop. */
 public class RouteAtStop internal constructor() {
 
-   /** Is this the first stop for most trips? */
-   public var isFirst: Boolean = true
-      internal set
+    internal constructor(
+        isFirst: Boolean,
+        isLast: Boolean,
+        stopsAtDirectionZero: Boolean,
+        stopsAtDirectionOne: Boolean,
+    ) : this() {
+        this.isFirst = isFirst
+        this.isLast = isLast
+        this.stopsAtDirectionZero = stopsAtDirectionZero
+        this.stopsAtDirectionOne = stopsAtDirectionOne
+    }
 
-   /** Is this the last stop for most trips? */
-   public var isLast: Boolean = true
-      internal set
+    /** Is this the first stop for most trips? */
+    public var isFirst: Boolean = true
+        internal set
 
-   /** Does this route stop when traveling in [DirectionId.Zero]? */
-   public var stopsAtDirectionZero: Boolean = false
-      internal set
+    /** Is this the last stop for most trips? */
+    public var isLast: Boolean = true
+        internal set
 
-   /** Does this route stop when traveling in [DirectionId.One]? */
-   public var stopsAtDirectionOne: Boolean = false
-      internal set
+    /** Does this route stop when traveling in [DirectionId.Zero]? */
+    public var stopsAtDirectionZero: Boolean = false
+        internal set
+
+    /** Does this route stop when traveling in [DirectionId.One]? */
+    public var stopsAtDirectionOne: Boolean = false
+        internal set
 }
