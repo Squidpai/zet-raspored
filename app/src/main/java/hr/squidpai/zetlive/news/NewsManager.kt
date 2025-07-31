7b17c21308969f5a71e037441ba3adb6f814f76c
Squidpai <leonardzadro@gmail.com>
2025-07-31 14:05:46 +0200

    Added NewsManager for managing news and traffic changes feeds.

--- app/src/main/java/hr/squidpai/zetlive/news/NewsManager.kt
+++ app/src/main/java/hr/squidpai/zetlive/news/NewsManager.kt
@@ -1,7 +1,48 @@
 package hr.squidpai.zetlive.news
 
+import com.prof18.rssparser.RssParser
+import com.prof18.rssparser.model.RssItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import java.io.File
+import kotlin.time.Duration.Companion.hours
+
+typealias NewsFeed = List<RssItem>
+
 object NewsManager {
 
+    const val NEWS_RSS_LINK = "https://www.zet.hr/rss_novosti.aspx"
+    private const val NEWS_FILE = "news.rss"
+    private val newsTimeToLive = 30.hours
 
+    const val TRAFFIC_CHANGES_RSS_LINK = "https://www.zet.hr/rss_promet.aspx"
+    private const val TRAFFIC_CHANGES_FILE = "traffic_changes.rss"
+    private val trafficChangesTimeToLive = 8.hours
+
+    private val parser = RssParser()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    lateinit var newsLoader: NewsLoader
+        private set
+
+    lateinit var trafficChangesLoader: NewsLoader
+        private set
+
+    fun init(filesDir: File) {
+        newsLoader = NewsLoader(
+            parser,
+            coroutineScope,
+            cachedFeedFile = File(filesDir, NEWS_FILE),
+            downloadLink = NEWS_RSS_LINK,
+            timeToLive = newsTimeToLive
+        )
+        trafficChangesLoader = NewsLoader(
+            parser,
+            coroutineScope,
+            cachedFeedFile = File(filesDir, TRAFFIC_CHANGES_FILE),
+            downloadLink = TRAFFIC_CHANGES_RSS_LINK,
+            timeToLive = trafficChangesTimeToLive,
+        )
+    }
 
 }
