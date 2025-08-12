fc1fd9d20ae9c48d37aa6de1d01e2f2385599cad
Squidpai <120787088+Squidpai@users.noreply.github.com>
2025-08-12 08:56:53 +0200

    Added Element.

---
+++ app/src/main/java/hr/squidpai/zetlive/news/Element.kt
@@ -0,0 +1,18 @@
+package hr.squidpai.zetlive.news
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.IntSize
+import java.io.Serializable
+
+sealed interface Element : Serializable {
+
+    @JvmInline
+    value class Text(val text: AnnotatedString) : Element {
+        companion object {
+            val Blank = Text(AnnotatedString(""))
+        }
+    }
+
+    data class Image(val sourceUrl: String, val altText: String?, val size: IntSize) : Element
+
+}
