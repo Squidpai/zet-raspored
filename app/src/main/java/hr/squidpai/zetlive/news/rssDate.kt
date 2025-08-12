b289ca84279f2a60155e8c87665e999f4791722a
Squidpai <120787088+Squidpai@users.noreply.github.com>
2025-08-12 09:04:20 +0200

    Added LocalDateTime.toFormattedString, that formats a date in a croatian style.

--- app/src/main/java/hr/squidpai/zetlive/news/rssDate.kt
+++ app/src/main/java/hr/squidpai/zetlive/news/rssDate.kt
@@ -3,6 +3,7 @@ package hr.squidpai.zetlive.news
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
+import java.util.Locale
 
 private val RFC_822_2_DIGIT_YEAR_DATE_TIME =
     DateTimeFormatter.ofPattern("EEE, dd MMM yy HH:mm:ss zzz")
@@ -13,3 +14,10 @@ val String.rssDate: LocalDateTime
     } catch (_: DateTimeParseException) {
         LocalDateTime.parse(this, RFC_822_2_DIGIT_YEAR_DATE_TIME)
     }
+
+private val localFormat = DateTimeFormatter.ofPattern(
+    "EEE, dd. MMMM yyyy., HH:mm",
+    Locale.forLanguageTag("hr")
+)
+
+fun LocalDateTime.toFormattedString(): String = format(localFormat)
