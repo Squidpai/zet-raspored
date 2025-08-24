2b241cb25dc49c55e596daca8a42b9b7128c0aa5
Squidpai <120787088+Squidpai@users.noreply.github.com>
2025-08-24 15:49:32 +0200

    Added documentation.

--- app/src/main/java/hr/squidpai/zetlive/ui/composables/HintBox.kt
+++ app/src/main/java/hr/squidpai/zetlive/ui/composables/HintBox.kt
@@ -11,26 +11,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import hr.squidpai.zetlive.Data
 
+/**
+ * A default implementation for a [TooltipBox] displaying a [hint].
+ *
+ * The `TooltipBox` isn't focusable, doesn't enable user input, and by default
+ * is visible if [Data.hints.shouldBeVisible] is `true` for the given `hint`,
+ * and is persistent.
+ *
+ * @param hint the hint to be displayed in the `TooltipBox`
+ * @param modifier the [Modifier] to be applied to the `TooltipBox`
+ * @param state handles the state of the tooltip's visibility; by default,
+ *              it is persistent, and is visible if `hint.shouldBeVisible()` is `true`
+ * @param content the composable that the tooltip will anchor to.
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HintBox(
-   hint: Data.hints,
-   modifier: Modifier = Modifier,
-   state: TooltipState = rememberTooltipState(
-      initialIsVisible = hint.shouldBeVisible(),
-      isPersistent = true,
-   ),
-   content: @Composable () -> Unit
+    hint: Data.hints,
+    modifier: Modifier = Modifier,
+    state: TooltipState = rememberTooltipState(
+        initialIsVisible = hint.shouldBeVisible(),
+        isPersistent = true,
+    ),
+    content: @Composable () -> Unit
 ) = TooltipBox(
-   positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
-   tooltip = {
-      RichTooltip(colors = TooltipDefaults.inverseRichTooltipColors()) {
-         Text(hint.hintText)
-      }
-   },
-   state,
-   modifier,
-   focusable = false,
-   enableUserInput = false,
-   content
+    positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+    tooltip = {
+        RichTooltip(colors = TooltipDefaults.inverseRichTooltipColors()) {
+            Text(hint.hintText)
+        }
+    },
+    state,
+    modifier,
+    focusable = false,
+    enableUserInput = false,
+    content
 )
--- app/src/main/java/hr/squidpai/zetlive/ui/composables/HintIconButton.kt
+++ app/src/main/java/hr/squidpai/zetlive/ui/composables/HintIconButton.kt
@@ -19,54 +19,66 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
 import kotlinx.coroutines.launch
 
+/**
+ * An icon button that displays a tooltip when clicked.
+ *
+ * @param icon The [ImageVector] to be displayed.
+ * @param contentDescription Text used by accessibility services to describe what this icon represents.
+ * @param tooltipTitle The title of the tooltip.
+ * @param tooltipText The text content of the tooltip.
+ * @param modifier The [Modifier] to be applied to the [TooltipBox].
+ * @param iconTint Tint to be applied to [icon].
+ * @param showClickIndication Whether to show a click indication (ripple effect) when the icon is clicked.
+ * @param action An optional composable to be displayed at the bottom of the tooltip, usually a button.
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HintIconButton(
-   icon: ImageVector,
-   contentDescription: String?,
-   tooltipTitle: String,
-   tooltipText: String,
-   modifier: Modifier = Modifier,
-   iconTint: Color = MaterialTheme.colorScheme.secondary,
-   showClickIndication: Boolean = false,
-   action: (@Composable () -> Unit)? = null,
+    icon: ImageVector,
+    contentDescription: String?,
+    tooltipTitle: String,
+    tooltipText: String,
+    modifier: Modifier = Modifier,
+    iconTint: Color = MaterialTheme.colorScheme.secondary,
+    showClickIndication: Boolean = false,
+    action: (@Composable () -> Unit)? = null,
 ) {
-   val state = rememberTooltipState(isPersistent = true)
-   TooltipBox(
-      positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
-      tooltip = {
-         RichTooltip(
-            title = { Text(tooltipTitle) },
-            action = action,
-            colors = TooltipDefaults.inverseRichTooltipColors(),
-         ) {
-            Text(tooltipText)
-         }
-      },
-      state,
-      modifier,
-      focusable = false,
-      enableUserInput = false,
-   ) {
-      val scope = rememberCoroutineScope()
-      Icon(
-         icon,
-         contentDescription,
-         modifier = Modifier
-            .minimumInteractiveComponentSize()
-            .run {
-               val onClick: () -> Unit = {
-                  if (!state.isVisible)
-                     scope.launch { state.show() }
-               }
+    val state = rememberTooltipState(isPersistent = true)
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+        tooltip = {
+            RichTooltip(
+                title = { Text(tooltipTitle) },
+                action = action,
+                colors = TooltipDefaults.inverseRichTooltipColors(),
+            ) {
+                Text(tooltipText)
+            }
+        },
+        state,
+        modifier,
+        focusable = false,
+        enableUserInput = false,
+    ) {
+        val scope = rememberCoroutineScope()
+        Icon(
+            icon,
+            contentDescription,
+            modifier = Modifier
+                .minimumInteractiveComponentSize()
+                .run {
+                    val onClick = {
+                        if (!state.isVisible)
+                            scope.launch { state.show() }
+                    }
 
-               if (showClickIndication) clickable(onClick = onClick)
-               else pointerInput(Unit) {
-                  while (true)
-                     detectTapGestures { onClick() }
-               }
-            },
-         iconTint,
-      )
-   }
+                    if (showClickIndication) clickable(onClick = onClick)
+                    else pointerInput(Unit) {
+                        while (true)
+                            detectTapGestures { onClick() }
+                    }
+                },
+            iconTint,
+        )
+    }
 }
