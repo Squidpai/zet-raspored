ff1798f1f4fab25f2bbe983fba2198cd95047e71
Squidpai <120787088+Squidpai@users.noreply.github.com>
2025-10-26 17:31:47 +0100

    Prettified code

--- app/src/main/java/hr/squidpai/zetlive/gtfs/Stop.kt
+++ app/src/main/java/hr/squidpai/zetlive/gtfs/Stop.kt
@@ -5,25 +5,23 @@ import hr.squidpai.zetapi.Stop
 import hr.squidpai.zetlive.ui.Symbols
 
 val Stop.label
-    get() =
-        Love.giveMeTheLabelForStop(id) ?: when {
-            routes.values.all { it.isFirst } ->
-                "Ulaz ${routes.keys.joinToString { it.shortName }}"
+    get() = Love.giveMeTheLabelForStop(id) ?: when {
+        routes.values.all { it.isFirst } ->
+            "Ulaz ${routes.keys.joinToString { it.shortName }}"
 
-            routes.values.all { it.isLast } ->
-                if (routes.size == 1)
-                    "Izlaz ${routes.keys.single().shortName}"
-                else "Izlaz"
+        routes.values.all { it.isLast } ->
+            if (routes.size == 1)
+                "Izlaz ${routes.keys.single().shortName}"
+            else "Izlaz"
 
-            else -> null
-        }
+        else -> null
+    }
 
 val Stop.iconInfo
-    get() =
-        when (Love.giveMeTheIconCodeForStop(id) % 10) {
-            1 -> Symbols.ArrowRightAlt to "Istok"
-            2 -> Symbols.ArrowLeftAlt to "Zapad"
-            3 -> Symbols.ArrowUpwardAlt to "Sjever"
-            4 -> Symbols.ArrowDownwardAlt to "Jug"
-            else -> null
-        }
+    get() = when (Love.giveMeTheIconCodeForStop(id) % 10) {
+        1 -> Symbols.ArrowRightAlt to "Istok"
+        2 -> Symbols.ArrowLeftAlt to "Zapad"
+        3 -> Symbols.ArrowUpwardAlt to "Sjever"
+        4 -> Symbols.ArrowDownwardAlt to "Jug"
+        else -> null
+    }
